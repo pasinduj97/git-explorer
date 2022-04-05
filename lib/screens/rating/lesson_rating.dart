a8194b4ce9621fa232ea8f9fd5201dbd6d1d0fb2
Avishka Shyaman <avishkashyaman@gmail.com>
2022-04-05 19:49:38 +0530

    changed lesson rating component

--- lib/screens/rating/lesson_rating.dart
+++ lib/screens/rating/lesson_rating.dart
@@ -1,12 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_rating_bar/flutter_rating_bar.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:git_explorer/components/forumComponents/rating_comment.dart';
+import 'package:git_explorer/services/rating_methods.dart';
 
 class LessonRating extends StatefulWidget {
   final snap;
+  final String lessonId;
+  final String subCategoryId;
+
   const LessonRating({
     Key? key,
     required this.snap,
+    required this.lessonId,
+    required this.subCategoryId,
   }) : super(key: key);
 
   @override
@@ -14,15 +22,46 @@ class LessonRating extends StatefulWidget {
 }
 
 class _LessonRatingState extends State<LessonRating> {
-  double rating = 3.3;
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  double allRating = 3.3;
+  bool isUserRated = false;
+  double currentUserRating = 0.0;
+  String currentUserComment = '';
 
   onRatingUpdate(rating) {
     setState(() {
-      this.rating = rating;
+      allRating = rating;
     });
   }
 
-// Bi5zNyHoGsYvPgoVvru2G8lpZJh1
+  onRatingSubmit() async {
+    if (isUserRated) {
+      await RatingMethods().updateRating(
+          comment: currentUserComment,
+          userId: _auth.currentUser!.uid,
+          rating: currentUserRating,
+          subCategoryId: widget.subCategoryId,
+          lessonId: widget.lessonId);
+    } else {
+      await RatingMethods().postRating(
+          comment: currentUserComment,
+          rating: currentUserRating,
+          subCategoryId: widget.subCategoryId,
+          lessonId: widget.lessonId);
+    }
+    Navigator.of(context).pop();
+  }
+
+  onRatingDetele() async {
+    await RatingMethods().deleteRating(
+        userId: _auth.currentUser!.uid,
+        subCategoryId: widget.subCategoryId,
+        lessonId: widget.lessonId);
+    Navigator.of(context).pop();
+  }
+
   showRatingDialog() => showDialog(
         context: context,
         builder: (context) => AlertDialog(
@@ -31,34 +70,81 @@ class _LessonRatingState extends State<LessonRating> {
             mainAxisSize: MainAxisSize.min,
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              Text('Tell others what you think'),
+              const Text('Tell others what you think'),
               const SizedBox(
-                height: 32,
+                height: 16,
               ),
               Center(
-                  child: RatingBar.builder(
-                initialRating: rating,
-                minRating: 1,
-                direction: Axis.horizontal,
-                allowHalfRating: true,
-                itemCount: 5,
-                itemSize: 30.0,
-                itemPadding: const EdgeInsets.symmetric(horizontal: 2.0),
-                itemBuilder: (context, _) => const Icon(
-                  Icons.star,
-                  color: Colors.blue,
+                child: RatingBar.builder(
+                  initialRating: currentUserRating,
+                  minRating: 2,
+                  direction: Axis.horizontal,
+                  allowHalfRating: true,
+                  itemCount: 5,
+                  itemSize: 30.0,
+                  itemPadding: const EdgeInsets.symmetric(horizontal: 2.0),
+                  itemBuilder: (context, _) => const Icon(
+                    Icons.star,
+                    color: Colors.blue,
+                  ),
+                  onRatingUpdate: (rating) {
+                    setState(() {
+                      currentUserRating = rating;
+                    });
+                  },
                 ),
-                onRatingUpdate: onRatingUpdate,
-              ))
+              ),
+              const Align(
+                child: Text(
+                  "Enter your title: ",
+                  style: TextStyle(fontSize: 12, color: Colors.black),
+                ),
+                alignment: Alignment.centerLeft,
+              ),
+              const SizedBox(
+                height: 12.0,
+              ),
+              TextFormField(
+                controller: TextEditingController(text: currentUserComment),
+                maxLines: 2,
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(5.0)),
+                  ),
+                  filled: false,
+                  focusedBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.blue, width: 1.0),
+                    borderRadius: BorderRadius.all(Radius.circular(5.0)),
+                  ),
+                ),
+                style: const TextStyle(
+                  fontSize: 14.0,
+                  color: Colors.black,
+                ),
+                onChanged: (_val) {
+                  // setState(() {
+                  currentUserComment = _val;
+                  // });
+                },
+                validator: (_val) {
+                  if (_val!.isEmpty) {
+                    return "Input field cannot be empty!";
+                  } else {
+                    return null;
+                  }
+                },
+              ),
             ],
           ),
           actions: <Widget>[
+            if (isUserRated)
+              TextButton(
+                child: const Text('Delete'),
+                onPressed: onRatingDetele,
+              ),
             TextButton(
-              child: const Text('Done'),
-              onPressed: () {
-                Navigator.of(context).pop();
-              },
-            ),
+                child: Text(isUserRated ? 'Update' : 'Submit'),
+                onPressed: onRatingSubmit),
           ],
         ),
       );
@@ -69,15 +155,27 @@ class _LessonRatingState extends State<LessonRating> {
     if (numberOfRating > 0) {
       double sumOfAllRating = 0.0;
       for (var rateRowItem in widget.snap) {
+        if (_auth.currentUser!.uid == rateRowItem.reference.id) {
+          setState(() {
+            currentUserRating = rateRowItem.get('rating').toDouble();
+            currentUserComment = rateRowItem.get('comment');
+            isUserRated = true;
+          });
+        }
+
         if (rateRowItem.get('rating') != null) {
           sumOfAllRating += rateRowItem.get('rating');
         }
       }
 
       setState(() {
-        rating = sumOfAllRating / numberOfRating;
+        allRating = sumOfAllRating / numberOfRating;
       });
     }
+
+    print('rated');
+    print(isUserRated);
+    print(_auth.currentUser!.uid);
 
     return Container(
         width: double.infinity,
@@ -111,14 +209,15 @@ class _LessonRatingState extends State<LessonRating> {
                       child: Column(
                           crossAxisAlignment: CrossAxisAlignment.start,
                           children: [
-                            Text("$rating",
+                            Text(
+                                "${double.parse((allRating).toStringAsFixed(1))}",
                                 style: const TextStyle(
                                   fontWeight: FontWeight.bold,
                                   fontSize: 38,
                                   color: Colors.white,
                                 )),
                             RatingBar.builder(
-                              initialRating: rating,
+                              initialRating: allRating,
                               minRating: 1,
                               direction: Axis.horizontal,
                               allowHalfRating: true,
@@ -130,9 +229,9 @@ class _LessonRatingState extends State<LessonRating> {
                                 Icons.star,
                                 color: Colors.blue,
                               ),
-                              onRatingUpdate: onRatingUpdate,
+                              onRatingUpdate: (rating) {},
                             ),
-                            Text("  Votes: 1,000",
+                            Text("  Votes: $numberOfRating",
                                 style: const TextStyle(
                                   fontWeight: FontWeight.bold,
                                   fontSize: 12,
@@ -147,7 +246,8 @@ class _LessonRatingState extends State<LessonRating> {
                             onPressed: () {
                               showRatingDialog();
                             },
-                            child: const Text('Add Your Rating'),
+                            child: Text(
+                                '${isUserRated ? 'Update' : 'Add'} Your Rating'),
                           ),
                           // RatingBar.builder(
                           //   initialRating: rating,
@@ -169,7 +269,9 @@ class _LessonRatingState extends State<LessonRating> {
             ),
             for (final ratingRowItem in widget.snap)
               RatingComment(
-                  ratingDoc: ratingRowItem.get('rating'),
+                  rating: ratingRowItem.get('rating'),
+                  date: ratingRowItem.get('timestamp').toDate(),
+                  id: ratingRowItem.reference.id,
                   comment: ratingRowItem.get('comment')),
           ],
         ));
