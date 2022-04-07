698ed454b83a73552a3050eeef3fe2d339dd57cf
Avishka Shyaman <avishkashyaman@gmail.com>
2022-04-07 18:33:20 +0530

    moved rating button as seperate widget

---
+++ lib/screens/rating/rating_button.dart
@@ -0,0 +1,219 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter_rating_bar/flutter_rating_bar.dart';
+import 'package:git_explorer/services/rating_methods.dart';
+
+class RatingButton extends StatefulWidget {
+  final String lessonId;
+  final String subCategoryId;
+  final bool isUserRated;
+  final double currentUserRating;
+  final String currentUserComment;
+
+  const RatingButton({
+    Key? key,
+    required this.lessonId,
+    required this.isUserRated,
+    required this.subCategoryId,
+    required this.currentUserRating,
+    required this.currentUserComment,
+  }) : super(key: key);
+
+  @override
+  State<RatingButton> createState() => _RatingButtonState();
+}
+
+class _RatingButtonState extends State<RatingButton> {
+  late FirebaseAuth _auth = FirebaseAuth.instance;
+
+  late TextEditingController controller;
+  // TextEditingController controller = TextEditingController();
+
+  double currentUserRating = 0.0;
+  String currentUserComment = '';
+
+  String codeDialog = '';
+  String valueText = '';
+
+  @override
+  void initState() {
+    super.initState();
+    print('user widget.isUserRated');
+    print(widget.isUserRated);
+    if (widget.isUserRated) {
+      currentUserRating = widget.currentUserRating;
+      currentUserComment = widget.currentUserComment;
+      controller = TextEditingController(text: widget.currentUserComment);
+    } else {
+      controller = TextEditingController();
+    }
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    print('RatingButton dispose');
+  }
+
+  onRatingSubmit() async {
+    // Navigator.of(context).pop();
+    if (widget.isUserRated) {
+      await RatingMethods().updateRating(
+          comment: controller.text,
+          userId: _auth.currentUser!.uid,
+          rating: currentUserRating,
+          subCategoryId: widget.subCategoryId,
+          lessonId: widget.lessonId);
+      controller.clear();
+    } else {
+      await RatingMethods().postRating(
+          comment: controller.text,
+          rating: currentUserRating,
+          subCategoryId: widget.subCategoryId,
+          lessonId: widget.lessonId);
+    }
+  }
+
+  onRatingDetele() async {
+    // Navigator.of(context).pop();
+    await RatingMethods().deleteRating(
+        userId: _auth.currentUser!.uid,
+        subCategoryId: widget.subCategoryId,
+        lessonId: widget.lessonId);
+    controller.clear();
+  }
+
+  showRatingDialog(context) => showDialog(
+        context: context,
+        builder: (context) => AlertDialog(
+          title: const Text('Rate this Post'),
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const Text('Tell others what you think'),
+              const SizedBox(
+                height: 16,
+              ),
+              Center(
+                child: RatingBar.builder(
+                  initialRating: currentUserRating,
+                  minRating: 2,
+                  direction: Axis.horizontal,
+                  allowHalfRating: true,
+                  itemCount: 5,
+                  itemSize: 30.0,
+                  itemPadding: const EdgeInsets.symmetric(horizontal: 2.0),
+                  itemBuilder: (context, _) => const Icon(
+                    Icons.star,
+                    color: Colors.blue,
+                  ),
+                  onRatingUpdate: (rating) {
+                    // setState(() {
+                    currentUserRating = rating;
+                    // });
+                  },
+                ),
+              ),
+              const Align(
+                child: Text(
+                  "Enter your title: ",
+                  style: TextStyle(fontSize: 12, color: Colors.black),
+                ),
+                alignment: Alignment.centerLeft,
+              ),
+              const SizedBox(
+                height: 12.0,
+              ),
+              TextField(
+                controller: controller,
+                // maxLines: 2,
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(5.0)),
+                  ),
+                  filled: false,
+                  focusedBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.blue, width: 1.0),
+                    borderRadius: BorderRadius.all(Radius.circular(5.0)),
+                  ),
+                ),
+                style: const TextStyle(
+                  fontSize: 14.0,
+                  color: Colors.black,
+                ),
+              ),
+            ],
+          ),
+          actions: <Widget>[
+            if (widget.isUserRated)
+              TextButton(
+                child: const Text('Delete'),
+                onPressed: () {
+                  onRatingDetele();
+                  Navigator.of(context).pop();
+                },
+              ),
+            TextButton(
+                child: Text(widget.isUserRated ? 'Update' : 'Submit'),
+                onPressed: () {
+                  onRatingSubmit();
+                  Navigator.of(context).pop();
+                }),
+          ],
+        ),
+      );
+
+  Future<void> _displayTextInputDialog(BuildContext context) async {
+    return showDialog(
+        context: context,
+        builder: (context) {
+          return AlertDialog(
+            title: Text('TextField in Dialog'),
+            content: TextField(
+              onChanged: (value) {
+                // setState(() {
+                valueText = value;
+                // });
+              },
+              controller: controller,
+              decoration: InputDecoration(hintText: "Text Field in Dialog"),
+            ),
+            actions: <Widget>[
+              FlatButton(
+                color: Colors.red,
+                textColor: Colors.white,
+                child: Text('CANCEL'),
+                onPressed: () {
+                  // setState(() {
+                  Navigator.pop(context);
+                  // });
+                },
+              ),
+              FlatButton(
+                color: Colors.green,
+                textColor: Colors.white,
+                child: Text('OK'),
+                onPressed: () {
+                  // setState(() {
+                  codeDialog = valueText;
+                  Navigator.pop(context);
+                  // });
+                },
+              ),
+            ],
+          );
+        });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      onPressed: () {
+        showRatingDialog(context);
+        // _displayTextInputDialog(context);
+      },
+      child: Text('${widget.isUserRated ? 'Update' : 'Add'} Your Rating'),
+    );
+  }
+}
